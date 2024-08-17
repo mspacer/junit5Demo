108257cd639f6f483fd2f6d1155915d3fef451ce
smalyshko <smalyshko@jt-soft.ru>
2024-08-17 18:40:16 +0300

    О jUnit5. Настройка проекта

---
+++ src/test/java/com/msp/junit/service/UserServiceTest.java
@@ -0,0 +1,12 @@
+package com.msp.junit.service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UserServiceTest {
+
+    @Test
+    void test() {
+        assertTrue(false);
+    }
+}
